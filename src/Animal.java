f72f96e831b2e66221ce9621cf301b75413c19fd
MykolaTurchn14 <tyrchnmike@gmail.com>
2021-08-18 08:20:26 +0300

    homework 1

---
+++ src/Animal.java
@@ -0,0 +1,56 @@
+//import lombok.*;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//
+//public class Animal {
+//    private String alias;
+//    private int age;
+//    private String animal;
+//
+//
+////    public Animal() {
+////    }
+////    public Animal(String alias, int age, String animal) {
+////        this.alias = alias;
+////        this.age = age;
+////        this.animal = animal;
+////    }
+////
+////    public String getAlias() {
+////        return alias;
+////    }
+////
+////    public void setAlias(String alias) {
+////        this.alias = alias;
+////    }
+////
+////    public int getAge() {
+////        return age;
+////    }
+////
+////    public void setAge(int age) {
+////        this.age = age;
+////    }
+////
+////    public String getAnimal() {
+////        return animal;
+////    }
+////
+////    public void setAnimal(String animal) {
+////        this.animal = animal;
+////    }
+////
+////    @Override
+////    public String toString() {
+////        return "Animal{" +
+////                "alias='" + alias + '\'' +
+////                ", age=" + age +
+////                ", animal='" + animal + '\'' +
+////                '}';
+////    }
+//}
+//
