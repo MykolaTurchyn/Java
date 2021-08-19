9f12da6ea9ad22447e8a6fc1ba9ea32b6aaed471
MykolaTurchn14 <tyrchnmike@gmail.com>
2021-08-19 19:02:23 +0300

    -10%

--- src/Main.java
+++ src/Main.java
@@ -20,14 +20,14 @@ public class Main {
         Engine engineCar1 = new Engine("BMW", 1.2, 333);
         SuperCar car1 = new SuperCar("BMW", "BMW", 2001, engineCar1);
 //        car1.getEngine().setVolume(2.5);
-        car1.getEngine().setVolume(car1.getEngine().getVolume() - 1.1);
+        car1.getEngine().setVolume(car1.getEngine().getVolume() * 0.1);
         System.out.println(car1);
 
         Engine engineCar2 = new Engine("VW", 2.0, 1212);
         SuperCar car2 = new SuperCar("WV", "WV", 2016, engineCar2);
         car2.getEngine().setVolume(3.0);
 
-        car2.getEngine().setVolume(car2.getEngine().getVolume()- 0.4);
+        car2.getEngine().setVolume(car2.getEngine().getVolume() - 0.4);
         System.out.println(car2);
     }
 }
