4823b215f758764f2247b635faec5f28e56f5e69
MykolaTurchn14 <tyrchnmike@gmail.com>
2021-09-01 23:08:40 +0300

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        ArrayList<User> userFromClient = new ArrayList<>();
+        userFromClient.add(new User(2, "kokos"));
+        userFromClient.add(new User(1, "ananas"));
+        userFromClient.add(new User(3, "banan"));
+        userFromClient.add(new User(5, "tomat"));
+        userFromClient.add(new User(4, "potatos"));
+        userFromClient.add(new User(6, "mango"));
 
+
+        String url = "jdbc:mysql://localhost:3306/first";
+        Connection connection = DriverManager.getConnection(url, "root", "rootroot");
+
+//        Statement statement = connection.createStatement();
+//        statement.executeUpdate("insert into user(name) values('sergiy')");
+//        System.out.println(statement);
+
+
+//        for (User user : userFromClient) {
+//            PreparedStatement preparedStatement = connection.prepareStatement("insert into user(name) values(?)");
+//            preparedStatement.setString(1, user.getName());
+//            preparedStatement.executeUpdate();
+//        }
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(2));
+        }
+        connection.close();
     }
 }
