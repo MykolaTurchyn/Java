5fab9768a6b6b57a483443e512d9acfac774b493
mykolaturchyn <tyrchnmikigt@gmail.com>
2021-09-16 13:32:24 +0300

    HW5 NOT DONE

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-import java.sql.*;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-
+        User Vasya = new User(1, "Vasya", "Popovych", "vasyapopovych@gamil.com", 30, Gender.MAIL);
+        User Olga = new User(3, "Olga", "Paperovych", "olgapaper@asd.com", 18, Gender.FEMALE);
+        User Sanya = new User(2, "Sanya", "Kokosovych", "sanyakokos@ukr.net.ua", 19, Gender.MAIL);
+        User Ira = new User(4, "Ira", "Sandorych", "irkasandorych@mai.ru", 10, Gender.FEMALE);
+        System.out.println(Vasya);
+        System.out.println(Olga);
+        System.out.println(Sanya);
+        System.out.println(Ira);
     }
 }
